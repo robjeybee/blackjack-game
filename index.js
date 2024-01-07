e9de1393dc043796703988b02adfc1841f54322d
Rob B <rob@rjrobbie.dev>
2024-01-07 19:20:43 +0000

    added new card button and onclick event listner

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-let firstCard = 1
-let secondCard = 1
+let firstCard = 1;
+let secondCard = 19;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isActivePlayer = true;
+let message = " ";
+let startEl = document.getElementById("start-game-el");
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el")
+let newCard = document.getElementById("new-card")
 
-let sum = firstCard + secondCard
+startEl.addEventListener("click", () => {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum;
+  if (sum < 21) {
+    message = "Call new card?";
+  } else if (sum === 21) {
+    message = "Blackjack!";
+    hasBlackjack = true;
+  } else {
+    message = "No win!";
+    isActivePlayer = false;
+  }
+  messageEl.textContent = message;
+});
 
-let hasBlackjack = false
-
-let isActivePlayer = true
-
-let message = " "
-
-
-if (sum < 21) {
-    message = "Call new card?"
-} else if (sum === 21) {
-    message = "Blackjack!"
-    hasBlackjack = true
-} else {
-    message = "No win!"
-    isActivePlayer = false
-}
-
-console.log(message)
-
+newCard.addEventListener("click", () => {
+    console.log("Drawing a new card from the deck!")
+})

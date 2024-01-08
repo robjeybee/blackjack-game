635d474ae01e5f8a964d430958186343c0dfa240
Rob B <rob@rjrobbie.dev>
2024-01-08 20:31:20 +0000

    created an array to store cards

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-let firstCard = 1;
-let secondCard = 19;
-let sum = firstCard + secondCard;
-let hasBlackjack = false;
-let isActivePlayer = true;
-let message = " ";
-let startEl = document.getElementById("start-game-el");
-let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
+let firstCard = 1
+let secondCard = 9
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackjack = false
+let isActivePlayer = true
+let message = " "
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let newCard = document.getElementById("new-card")
 
-startEl.addEventListener("click", () => {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent = "Sum: " + sum;
+let startGame = () => {
+  renderGame()
+}
+
+let renderGame = () => {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
   if (sum < 21) {
-    message = "Call new card?";
+    message = "Call new card?"
   } else if (sum === 21) {
-    message = "Blackjack!";
-    hasBlackjack = true;
+    message = "Blackjack!"
+    hasBlackjack = true
   } else {
-    message = "No win!";
-    isActivePlayer = false;
+    message = "No win!"
+    isActivePlayer = false
   }
-  messageEl.textContent = message;
-});
+  messageEl.textContent = message
+};
 
-newCard.addEventListener("click", () => {
-    console.log("Drawing a new card from the deck!")
-})
+let newCard = () => {
+  let card = 11
+  sum += card
+  cards.push(card)
+  console.log(cards)
+  renderGame()
+}
+  

e4d8e614a418dad1b5d05b5949d8b994ef906c07
Rob B <rob@rjrobbie.dev>
2024-01-12 22:03:08 +0000

    mplemented conditional if statements that uses booleans to determine if a new card is drawn from my blackjack deck

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let cards = []
-let sum = 0
-let hasBlackjack = false
-let isActivePlayer = false
-let message = " "
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let cards = [];
+let sum = 0;
+let hasBlackjack = false;
+let isActivePlayer = false;
+let message = " ";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard() {
-  let randomCardNumber = Math.floor(Math.random() * 13 + 1)
+  let randomCardNumber = Math.floor(Math.random() * 13 + 1);
   if (randomCardNumber === 1) {
     return 11;
   } else if (randomCardNumber >= 11 && randomCardNumber <= 13) {
@@ -19,13 +19,14 @@ function getRandomCard() {
 }
 
 function startGame() {
-  isActivePlayer = true
-  let firstCard = getRandomCard()
-  let secondCard = getRandomCard()
-  cards = [firstCard, secondCard]
-  sum = firstCard + secondCard
+  isActivePlayer = true;
+  hasBlackjack = false
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
-};
+}
 
 let renderGame = () => {
   cardsEl.textContent = "Cards: ";
@@ -49,8 +50,10 @@ let renderGame = () => {
 };
 
 let newCard = () => {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isActivePlayer === true && hasBlackjack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 };
